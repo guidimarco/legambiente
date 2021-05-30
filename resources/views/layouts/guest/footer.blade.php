8a9be337d5112b5554f44fe16f01d13f8876dfa4
Marco Guidi <mark.88g@gmail.com>
2021-05-30 17:28:18 +0200

    next responsive

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -1,6 +1,7 @@
 <footer>
     <div>
-        <h2 id="footer-title">Legambiente Capannori e Piana Lucchese --> </h2>
+        <h2 id="footer-title" class="legambiente">Legambiente Capannori e Piana Lucchese --> </h2>
+        <h2 id="footer-title" class="seguici">Seguici --> </h2>
         <ul id="social">
             <li><a target="_blank" href="https://www.facebook.com/legambientecapannoriepianadilucca/"><i class="fab fa-facebook-square"></i></a></li>
             <li><a target="_blank" href="https://www.instagram.com/legambientetoscana/"><i class="fab fa-instagram"></i></a></li>
