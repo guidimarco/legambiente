1bde145155a08af57a918f1c6ecbbb05d67c4dd5
Marco Guidi <mark.88g@gmail.com>
2021-05-30 17:40:08 +0200

    minor changes

--- resources/views/next.blade.php
+++ resources/views/next.blade.php
@@ -4,7 +4,7 @@
 <div id="next-page" class="container-fluid">
     <div class="row">
         <div class="col-12 text-center">
-            <h1>I prossimi eventi</h1>
+            <h1>--> I prossimi eventi <--</h1>
         </div>
         <div class="col-12">
             <div id="carouselWrapper" class="carousel slide" data-ride="carousel">
@@ -16,27 +16,42 @@
                 </ol>
                 <!-- Events -->
                 <div class="carousel-inner">
+                    <!-- Single event -->
                     <div class="carousel-item active">
                         <img class="d-block w-100" src="img/1.jpeg" alt="First slide">
                         <div class="carousel-caption d-none d-md-block">
                             <h5 class="text-uppercase">27 Maggio 18:00 --> 20:00</h5>
                             <h6 class="text-uppercase">Workshop su ambiente riuso ed economia circolare</h6>
                             <p class="text-uppercase">Piazza "Aldo Moro"</p>
+                            <!-- icons -->
+                            <div class="carousel-icons">
+                                <a href="img/1.jpeg" alt="scarica l'evento" download><i class="fas fa-file-download"></i></a>
+                            </div>
                         </div>
                     </div>
+                    <!-- Single event -->
                     <div class="carousel-item">
                         <img class="d-block w-100" src="img/2.jpeg" alt="Second slide">
                         <div class="carousel-caption d-none d-md-block">
                             <h5 class="text-uppercase">29 Maggio</h5>
                             <h6 class="text-uppercase">Il sabato dell'ambiente</h6>
+                            <!-- icons -->
+                            <div class="carousel-icons">
+                                <a href="img/2.jpeg" alt="scarica l'evento" download><i class="fas fa-file-download"></i></a>
+                            </div>
                         </div>
                     </div>
+                    <!-- Single event -->
                     <div class="carousel-item">
                         <img class="d-block w-100" src="img/3.jpeg" alt="Third slide">
                         <div class="carousel-caption d-none d-md-block">
                             <h5 class="text-uppercase">29 Maggio</h5>
                             <h6 class="text-uppercase">Smart School</h6>
                             <p class="text-uppercase">Fondazione Campus</p>
+                            <!-- icons -->
+                            <div class="carousel-icons">
+                                <a href="img/3.jpeg" alt="scarica l'evento" download><i class="fas fa-file-download"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
