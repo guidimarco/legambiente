1d66f26f9c60258a7a22cf08fce50188951e23bb
guidimarco <mark.88g@gmail.com>
2021-06-23 21:35:00 +0200

    New seeder

--- database/seeds/UsersSocialTableSeeder.php
+++ database/seeds/UsersSocialTableSeeder.php
@@ -1,10 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\User;
-use Illuminate\Support\Facades\Hash;
 
-class UsersTableSeeder extends Seeder
+class UsersSocialTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
