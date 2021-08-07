0cc163b0d76658fdb7820e63e3d864dd3ff14b12
guidimarco <mark.88g@gmail.com>
2021-08-07 22:24:57 +0200

    Add welcome description and restyling

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,22 @@
 @section('title', 'Legambiente Capannori | Welcome')
 
 @section('content')
-<h1 id="main-title">
-    Legambiente<br/>
-    Capannori e<br/>
-    Piana Lucchese
-</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div id="welcome-wrapper">
+                <div id="welcome-container">
+                    <h1 id="main-title">Legambiente Capannori e Piana Lucchese</h1>
+                    <div class="main-text text-left">
+                        <p>Siamo un’associazione senza fini di lucro, fatta di cittadini e cittadine che hanno a cuore la tutela dell’ambiente in tutte le sue forme, la qualità della vita, una società più equa, giusta e solidale.</p>
+                        <p>Un grande movimento apartitico fatto di persone che, attraverso il volontariato e la partecipazione diretta, si fanno promotori del cambiamento per un futuro migliore.</p>
+                        <p>Abbiamo fondato la nostra missione sull’am­bientalismo scientifico, raccogliendo dal bas­so migliaia di dati sul nostro ecosistema, che sono alla base di ogni denuncia e proposta.</p>
+                        <p>Da 40 anni ci battiamo per un mondo migliore, combattendo contro l’inquinamento, l’illegalità e l’ingiustizia per la bellezza, la tutela, una migliore qualità della vita.</p>
+                        <p>Abbiamo vinto tante battaglie. Ci attendono nuove sfide e c’è sempre più bisogno di noi. Ma per continuare ad essere noi abbiamo bisogno di TE.</p>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </div>
+</div>
 @endsection
